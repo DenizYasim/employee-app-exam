c9947453434dd6cebc44bb4bef9c664ed926d11b
Deniz Yasim <deniz.yasim01@gmail.com>
2023-12-17 19:00:01 +0200

    Added unoptimised logic for mutual project worktime, new dataUtils function, TODO Componentise

--- employee-app-exam/src/components/DataVisualise/DataVisualise.jsx
+++ employee-app-exam/src/components/DataVisualise/DataVisualise.jsx
@@ -6,7 +6,7 @@ import {
 } from "../../utils/dataUtils";
 import FileUploadInput from "../FileUpload/FileUploadInput";
 import DataTable from "../DataTable/DataTable";
-import dateDifferenceInDays from "../../utils/dateDifferenceInDays";
+import { dateDifferenceInDays } from "../../utils/dateDifferenceInDays";
 
 function DataVisualise() {
 	const [data, setData] = useState({});
@@ -45,10 +45,36 @@ function DataVisualise() {
 				console.log(grouped);
 
 				for (let key in grouped) {
+					const dateWorkedTogether = [];
+					const employeesWorkingTogether = [];
+					let earliestDate = Number.MAX_SAFE_INTEGER;
+					let latestDate = 0;
+
+					if (grouped[key].length > 1) {
+						for (let i = 0; i < grouped[key].length; i++) {
+							for (let j = i + 1; j < grouped[key].length; j++) {
+								console.log(grouped[key][i][2]);
+								console.log(grouped[key][j][2]);
+							}
+						}
+					}
+
 					grouped[key].forEach((value) => {
-						console.log(value[2]);
-						console.log(dateDifferenceInDays(value[2], value[3]));
+						if (value[2] < earliestDate) {
+							earliestDate = value[2];
+							dateWorkedTogether[0] = earliestDate;
+						}
+
+						if (value[3] > latestDate) {
+							latestDate = value[3];
+							dateWorkedTogether[1] = latestDate;
+						}
+						// console.log(value[2]);
+						// console.log(dateDifferenceInDays(value[2], value[3]));
 					});
+					console.log(
+						dateDifferenceInDays(dateWorkedTogether[0], dateWorkedTogether[1])
+					);
 				}
 			};
 		}
